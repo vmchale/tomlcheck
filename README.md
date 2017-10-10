ff037d3928750eccac51c2d3770e9b55ca1b45de
Vanessa McHale <vanessa.mchale@reconfigure.io>
2017-10-10 13:59:01 -0500

    travis

--- README.md
+++ README.md
@@ -1,5 +1,21 @@
 # tomlcheck
 
+`tomlcheck` is a command-line wrapper around the `htoml` library which can be
+used as a syntax checker for TOML.
+
+You can find a vim plugin [here](link goes here).
+
 ## Installation
 
-## Configuration
+### Binaries
+
+Head over to the [release page](link goes here) to see if your platform has
+binaries. Simply put them on your path and you should be good to go.
+
+### Stack
+
+Install [stack](https://docs.haskellstack.org/en/stable/README/), then
+
+```bash
+ $ stack install tomlcheck --resolver nightly
+```
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,5 +1,7 @@
+{-# LANGUAGE DataKinds         #-}
 {-# LANGUAGE DeriveGeneric     #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeOperators     #-}
 
 module Lib
     ( exec
@@ -10,7 +12,7 @@ import           GHC.Generics    (Generic)
 import           Options.Generic
 import           Text.Toml       (parseTomlDoc)
 
-data Program = Program { file :: FilePath }
+data Program = Program { file :: FilePath <?> "Path to file to be checked." }
     deriving (Generic)
 
 instance ParseRecord Program
@@ -18,7 +20,7 @@ instance ParseRecord Program
 exec :: IO ()
 exec = do
     x <- getRecord "Command-line wrapper around htoml"
-    let path = file x
+    let path = unHelpful $ file x
     contents <- TIO.readFile path
     case parseTomlDoc path contents of
         Right{} -> pure ()
